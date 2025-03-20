762d98daf841f079bfc8869ade35fd806d732cca
Kazuki Obata <koiru3822fs@gmail.com>
2025-03-20 22:22:19 +0900

    bench resource adding (#12)
    * bench resource adding
    
    * resource adding in k distributing
    
    * remove useless comments

--- benches/hash_ring.rs
+++ benches/hash_ring.rs
@@ -1,20 +1,25 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use rand::Rng;
 
 use hash_bench::hash_ring::{HashRing, HashRingInterface};
 
-fn bench_hash_ring(c: &mut Criterion) {
-    c.bench_function("bench_hash_ring", |b| {
-        b.iter(|| {
-            let k = 5;
+fn bench_hash_ring_resource_adding(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hash_ring_resource_adding");
+
+    for k in 2u32..=16 {
+        group.bench_with_input(BenchmarkId::from_parameter(k), &k, |b, &k| {
+            let mut h = HashRing::new(k);
             let n = (2 as i32).pow(k);
-            for i in 1..n {
-                let mut h = HashRing::new(k);
-                h.add_node(i);
-            }
-            std::hint::black_box(());
+            h.add_node(1);
+            b.iter(|| {
+                let mut rng = rand::rng();
+                std::hint::black_box(h.add_resource(rng.random_range(0..n)));
+            });
+            h.remove_node(1);
         });
-    });
+    }
+    group.finish();
 }
 
-criterion_group!(benches, bench_hash_ring,);
+criterion_group!(benches, bench_hash_ring_resource_adding,);
 criterion_main!(benches);
