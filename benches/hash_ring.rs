724b55f728474738237721e393b2296fa14a6a25
donkomura <koiru3822fs@gmail.com>
2025-03-20 20:32:00 +0900

    remove useless comments

--- benches/hash_ring.rs
+++ benches/hash_ring.rs
@@ -9,15 +9,11 @@ fn bench_hash_ring_resource_adding(c: &mut Criterion) {
     for k in 2u32..=16 {
         group.bench_with_input(BenchmarkId::from_parameter(k), &k, |b, &k| {
             let mut h = HashRing::new(k);
-            let n = (2 as i32).pow(k); // means 2^k
+            let n = (2 as i32).pow(k);
             h.add_node(1);
             b.iter(|| {
                 let mut rng = rand::rng();
-                std::hint::black_box(
-                    // adding a new node 2 means that
-                    // almost of all resource (2^k-1) in the cluster will be moved to the new node 2
-                    h.add_resource(rng.random_range(0..n)),
-                );
+                std::hint::black_box(h.add_resource(rng.random_range(0..n)));
             });
             h.remove_node(1);
         });
